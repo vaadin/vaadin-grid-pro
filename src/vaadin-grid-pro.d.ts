0a4d51640444c397b807561aa7ee89864ec152ae
web-padawan <iamkulykov@gmail.com>
2020-12-18 14:15:00 +0200

    Add type definitions for custom events

--- src/vaadin-grid-pro.d.ts
+++ src/vaadin-grid-pro.d.ts
@@ -1,6 +1,39 @@
-import { GridElement } from '@vaadin/vaadin-grid/src/vaadin-grid.js';
+import { GridElement, GridElementEventMap } from '@vaadin/vaadin-grid/src/vaadin-grid';
+
+import { GridItem } from '@vaadin/vaadin-grid';
 
 import { InlineEditingMixin } from './vaadin-grid-pro-inline-editing-mixin.js';
+
+/**
+ * Fired when the user starts editing a grid cell.
+ */
+export type GridProCellEditStarted = CustomEvent<{
+  value: {
+    index: number;
+    item: GridItem;
+    path: string;
+  };
+}>;
+
+/**
+ * Fired before exiting the cell edit mode, if the value has been changed.
+ */
+export type GridProItemPropertyChanged = CustomEvent<{
+  value: {
+    index: number;
+    item: GridItem;
+    path: string;
+    value: string | boolean;
+  };
+}>;
+
+export interface GridProElementEventMap {
+  'cell-edit-started': GridProCellEditStarted;
+
+  'item-property-changed': GridProItemPropertyChanged;
+}
+
+export interface GridProEventMap extends HTMLElementEventMap, GridProElementEventMap, GridElementEventMap {}
 
 /**
  * `<vaadin-grid-pro>` is a high quality data grid / data table Web Component with extended functionality.
@@ -15,6 +48,18 @@ import { InlineEditingMixin } from './vaadin-grid-pro-inline-editing-mixin.js';
  */
 declare class GridProElement extends InlineEditingMixin(GridElement) {
   static _finalizeClass(): void;
+
+  addEventListener<K extends keyof GridProEventMap>(
+    type: K,
+    listener: (this: GridProElement, ev: GridProEventMap[K]) => void,
+    options?: boolean | AddEventListenerOptions
+  ): void;
+
+  removeEventListener<K extends keyof GridProEventMap>(
+    type: K,
+    listener: (this: GridProElement, ev: GridProEventMap[K]) => void,
+    options?: boolean | EventListenerOptions
+  ): void;
 }
 
 declare global {
